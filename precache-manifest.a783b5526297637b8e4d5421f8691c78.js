d5df9cd463ef1108823257ee0ed91eef87ff118d
Shan Wong <shan.wong29@gmail.com>
2020-04-25 11:25:04 +0200

    Updates

---
+++ precache-manifest.a783b5526297637b8e4d5421f8691c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c104b791b46c143e0e1da4853cc0889",
+    "url": "/Github-repos-checker/index.html"
+  },
+  {
+    "revision": "ecce333cb6680ddfdcbc",
+    "url": "/Github-repos-checker/static/css/main.17b3d90e.chunk.css"
+  },
+  {
+    "revision": "160a7950c61fc68fc4a4",
+    "url": "/Github-repos-checker/static/js/2.b1059880.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/Github-repos-checker/static/js/2.b1059880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecce333cb6680ddfdcbc",
+    "url": "/Github-repos-checker/static/js/main.46b025c8.chunk.js"
+  },
+  {
+    "revision": "d6bce137c13af1ca1b88",
+    "url": "/Github-repos-checker/static/js/runtime-main.d482fb45.js"
+  }
+]);
